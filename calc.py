da1c5c8b74204b5749013786f5dcf8a0e874a43c
inhahe <inhahe@gmail.com>
2016-04-01 18:51:01 -0400

    Fixed a bug.

--- calc.py
+++ calc.py
@@ -1,7 +1,13 @@
 from __future__ import division
 import sys
 from math import *
-print "{:,d}".format(eval(''.join(sys.argv[1:])))
+v = eval(''.join(sys.argv[1:]))
+if type(v) is int:
+  print "{:,d}".format(v)
+elif type(v) is float:
+  print "{:,f}".format(v)
+else:
+  print v
 
 
     
